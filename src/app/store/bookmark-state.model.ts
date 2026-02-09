d6ef7a2bea03edbcdd107b8507ad617557b65446
Lucian Voiculescu <voiculescu.lucian.smp@gmail.com>
2026-02-09 16:06:54 +0200

    remove not needed model

--- src/app/store/bookmark-state.model.ts
+++ src/app/store/bookmark-state.model.ts
@@ -1,12 +1,5 @@
 import { Bookmark } from "../interfaces/bookmark.interface";
 
-export interface BookmarkState {
-    id: number;
-    title: string;
-    url: string;
-    createdAt: string;
-}
-
 export type CreateBookmarkPayload = Omit<
   Bookmark,
   'id' | 'createdAt'
